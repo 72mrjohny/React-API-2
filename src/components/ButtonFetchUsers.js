bff10d96793303bf1a5b06a1e8b52445bf6de73e
Jan Pieniążek <johnn93@wp.pl>
2019-08-18 18:20:12 +0200

    added picture and styles

--- src/components/ButtonFetchUsers.js
+++ src/components/ButtonFetchUsers.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const ButtonFetchUsers = props => {
     return (
-        <button onClick={props.click}>Wczytaj 5 użytkowników</button>
+        <button style={{
+            padding: '10px 20px',
+            border: '2px solid black',
+            backgroundColor: 'white',
+            margin: 20,
+            textTransform: 'uppercase'
+        }} onClick={props.click}>Wczytaj 5 użytkowników</button>
     );
 }
-
 export default ButtonFetchUsers;
